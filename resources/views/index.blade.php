fe27dd142bc29f0761ef244491686b54f3ee3030
Dibya Darshan Khanal <44671452+Dibae101@users.noreply.github.com>
2022-04-01 01:21:03 +0545

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="jumbotron">
-        <h1>Forum App</h1>
+        <h1>Git WebHook on Jenkins</h1>
         <p class="lead">
             This is a simple web application developed with Laravel PHP Framework. <br>
             It is made for educational purpose at <a href="https://daredevops.com">Dare DevOps</a>
